dc87367783383bfc8c35decac70a0bd52879df90
akasataikisiti <nama0you0chu@gmail.com>
2023-05-17 04:18:24 +0900

    レイアウト作成
    サイドバー作成。
    index.jsに表示させていたユーザ情報をレイアウトのサイドバーに移した

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
 export const AppLayout = ({ children }) => {
+  const { user } = useUser();
   return (
-    <div className="grid grid-cols-[300px_1fr]">
-      <div className="bg-pink-500">this is the app layout</div>
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-slate-800">
+          <div>logo</div>
+          <div>cta button</div>
+          <div>tokens</div>
+        </div>
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+          list of posts
+        </div>
+        <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+          {!!user ? (
+            <>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                height={50}
+                width={50}
+                className="rounded-full"
+              />
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <Link href="/api/auth/login">Login</Link>
+          )}
+        </div>
+      </div>
       <div className="bg-yellow-500">{children}</div>
     </div>
   );
