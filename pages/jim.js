feb9769dc05b22b87e2f6deb3e47e3adbff69677
akasataikisiti <nama0you0chu@gmail.com>
2023-05-17 01:41:57 +0900

    authボタン作成

---
+++ pages/jim.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>jim pages</h1>
+    </div>
+  );
+}
---
+++ pages/post/[postid].js
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>this is new post page</h1>
+    </div>
+  );
+}
