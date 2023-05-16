8e80b462e84b5709b3abcb3c4d1d076c626fb659
akasataikisiti <nama0you0chu@gmail.com>
2023-05-17 02:38:28 +0900

    ログイン機能作成
    getServerSidePropsとwithPageAuthRequiredを作って
    未ログイン時に閲覧できないページを未ログイン状態で開こうとした時に
    Auth0のログイン画面に飛ばす様にした。

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,7 +1,18 @@
-export default function NewPost() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function NewPost(props) {
+  console.log("New POST PROPS: ", props);
   return (
     <div>
       <h1>this is new post page</h1>
     </div>
   );
 }
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {
+      test: "this is a test",
+    },
+  };
+});
