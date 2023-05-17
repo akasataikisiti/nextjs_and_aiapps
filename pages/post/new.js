8db1c9b5050f5895f96fc5ab335256e79ec76da1
akasataikisiti <nama0you0chu@gmail.com>
2023-05-17 20:30:55 +0900

    topiとkeywordsをクライアントから渡す様にした

--- pages/post/new.js
+++ pages/post/new.js
@@ -4,10 +4,17 @@ import { AppLayout } from "../../components/AppLayout/AppLayout";
 
 export default function NewPost(props) {
   console.log("New POST PROPS: ", props);
+  const [topic, setTopic] = useState("");
+  const [keywords, setKeywords] = useState("");
   const [postContent, setPostContent] = useState("");
-  const handleClick = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const response = await fetch(`/api/generatePost`, {
       method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ topic, keywords }),
     });
     const json = await response.json();
     console.log("RESULT", json.post.postContent);
@@ -15,10 +22,31 @@ export default function NewPost(props) {
   };
   return (
     <div>
-      <h1>this is new post page</h1>
-      <button className="btn" onClick={handleClick}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
       <div
         className="max-w-screen-sm p-10"
         dangerouslySetInnerHTML={{ __html: postContent }}
