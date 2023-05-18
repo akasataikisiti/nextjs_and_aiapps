ee1370047cab93c05f949715b50dd713fad19167
akasataikisiti <nama0you0chu@gmail.com>
2023-05-18 19:33:43 +0900

    記事生成エンドポイント作成
    動的URLパラメータでMongoDBの記事データのObjectIDで検索を掛けて
    見つかったらその内容を返却
    なかったらリダイレクト

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -1,6 +1,10 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { ObjectId } from "mongodb";
+import { AppLayout } from "../../components/AppLayout/AppLayout";
+import clientPromise from "../../lib/mongodb";
 
-export default function Post() {
+export default function Post(props) {
+  console.log("PROPS :", props);
   return (
     <div>
       <h1>this is new post page</h1>
@@ -8,8 +12,40 @@ export default function Post() {
   );
 }
 
-export const getServerSideProps = withPageAuthRequired(() => {
-  return {
-    props: {},
-  };
+// ページのレイアウトを決定するために使用される
+Post.getLayout = (page, pageProps) => {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("BlogStandard");
+    const user = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(ctx.params.postid),
+      userId: user._id,
+    });
+
+    if (!post) {
+      return {
+        redirect: {
+          destination: "/post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        postContent: post.postContent,
+        title: post.title,
+        metaDescription: post.metaDescription,
+        keywords: post.keywords,
+      },
+    };
+  },
 });
